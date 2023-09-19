57984856d7031c24beaa28934b92a9f8af362821
WonhwaGal <110241714+WonhwaGal@users.noreply.github.com>
2023-09-19 12:57:58 +0300

    LargeCommit_10

---
+++ Assets/Scripts/Camera/AimPointer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AimPointer
+{
+    private Vector3 _camPosition;
+    private float _size;
+    private GameObject _pointer;
+
+    public void Init(float size, GameObject pointer)
+    {
+        _size = size;
+        _pointer = pointer;
+    }
+
+    public void ShowPointer()
+    {
+        float scale = Vector3.Distance(_pointer.transform.position, _camPosition);
+        _pointer.transform.localScale = Vector3.one * scale * _size;
+        _pointer.SetActive(true);
+    }
+
+    public void HidePointer() => _pointer.SetActive(false);
+}
